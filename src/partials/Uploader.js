6a5be944afdc5a0bfd5ea6eb13852698e827b543
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-01-18 10:52:03 +0300

    Reading csv file

--- src/partials/Uploader.js
+++ src/partials/Uploader.js
@@ -1,12 +1,83 @@
 import { useState } from 'react'
 
 const Uploader =  () => {
-    const [ status, setStatus ] = useState(false)
+    const [ json, setJson ] = useState()
+
+    /**
+     * @param {Object} event An event triggered by the onClick event handler of the upload button element.
+     * @returns {Object} JSON object that has an array of items, each containing the fields specified by the user.
+     */
+    const handleJSON = async (event) =>  {
+        const { users } = JSON.parse(event.target.result)
+        console.log(users)
+
+        const cleanedFileContent = []
+        for(let i = 0; i < users.length; i++) {  
+            if(users[i]?.email && users[i]?.name) {
+                const { email, name, password="changeit"} = users[i]
+                cleanedFileContent.push({name: name, email: email, password: password})
+            } 
+        }
+        return await cleanedFileContent    
+    }
+
+
+
+    /**
+     * @param {Object} event An event triggered by the onClick event handler of the upload button element.
+     * @returns {Object} JSON object that has an array of items, each containing the fields specified by the user.
+     */
+
+    const handleCsv = async (event) => {
+        const rows = event.target.result.split("\n")
+        let fields = rows[0].split(";")
+        const users  = []
+        let emailIndex=fields.indexOf("Login email"), passwordIndex=fields.indexOf("One-time password"), firstNameIndex= fields.indexOf("First name"), lastNameIndex=fields.indexOf("Last name")
+    
+        for(let i = 1; i < rows.length; i++){
+            const row = rows[i].split(";")
+            users.push({name: `${row[firstNameIndex]} ${row[lastNameIndex]}`, email:row[emailIndex], password:row[passwordIndex]})
+        }
+        return await users  
+    }
+
+    
+    /**
+     * @param {String} importedElement The id of the element where the user selects the element
+     * @returns {Object} JSON object that has an array of items, each containing the fields specified by the user.
+     */
+    const importFile = (importedElement) => {
+        const importedFile = document.getElementById(importedElement).files[0]
+        const reader = new FileReader()
+        const extension = importedFile.name.split('.').pop()
+        
+        reader.onload = async function(event) {
+            if(extension === 'csv') {
+                setJson(await handleCsv(event))
+            }
+
+            if(extension === 'json') {
+                setJson(await handleJSON(event))
+            }
+        }
+        
+        reader.readAsText(importedFile)
+        console.log(importedFile.name.split('.').pop())
+        console.log(importedFile.type)
+        console.log(json)    
+    }
 
 
     return (
-        <div>
-            <input type="file" name="file"/>    
+        <div style={{display: "flex", gap:"20px"}}>
+            <input id="imported-element"type="file" name="file" accept=".csv, .sql, .json"/>    
+            <button onClick={()=>{
+                importFile("imported-element")
+            }}>Upload </button>
+            <button onClick={()=> {
+                console.log(json)
+            }}>log data</button>
+
         </div>
     )
 }
